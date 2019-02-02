8d1bc844cace1dd5148b5f931825a9f7854dac43
Samuel Busson <samuel.busson@hotmail.fr>
2019-02-02 11:18:08 -0500

    Correct

--- Devoir1/main.py
+++ Devoir1/main.py
@@ -5,20 +5,16 @@ file = pd.read_csv("crimeMontreal.csv", encoding = "ISO-8859-1")
 num_ligne = file.shape[0]
 num_column = file.shape[1]
 
-incomplete_columun = []
-
-print("Le fichier possède", num_ligne, "lignes et", file.shape[1], "colonnes")
+print("Le fichier possède", num_ligne, "observations et", file.shape[1], "propriétés")
 
 for column in file:
     count_lines = file[column].isnull().sum()
-    print("Sur les", num_ligne, "lignes, il y a", count_lines , "lignes avec des données manquantes dans la colonne", column)
-    if count_lines > 0:
-        incomplete_columun.append(column)
-
-for column in incomplete_columun:
-   file[column].dropna(inplace = True)
+    print("Sur les", num_ligne, "lignes, il y a", count_lines , "ligne(s) avec des données manquantes dans la colonne", column)
 
 
-#Vérifier la valeur de retour
 
+print("\nPBQ correspond au numéro du poste de police, aucun poste n'a le numéro -1, on remplace donc par ce dernier\n")
+file = file.fillna(-1)
 
+print("Pour plus de clarté on remplace \"dans / sur\" par \"du contenu\"  \n")
+file["CATEGORIE"] = file["CATEGORIE"].str.replace('dans / sur véhicule à moteur', 'du contenu d\'un véhicule')
