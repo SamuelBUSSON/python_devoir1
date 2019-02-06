bf116ebbea6900acb6c5aef871f6848da341bc74
Samuel Busson <samuel.busson@hotmail.fr>
2019-02-06 17:24:25 -0500

    Pie Diagram

--- Devoir1/main.py
+++ Devoir1/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 file = pd.read_csv("crimeMontreal.csv", encoding = "ISO-8859-1")
 num_ligne = file.shape[0]
@@ -18,3 +19,21 @@ file = file.fillna(-1)
 
 print("Pour plus de clarté on remplace \"dans / sur\" par \"du contenu\"  \n")
 file["CATEGORIE"] = file["CATEGORIE"].str.replace('dans / sur véhicule à moteur', 'du contenu d\'un véhicule')
+
+
+labels = file["CATEGORIE"].unique()
+sizes = []
+totalValue = 0
+for lab in labels:
+    curVal = file[file["CATEGORIE"] == lab].shape[0]
+    totalValue = totalValue + curVal
+    sizes.append(curVal)
+
+print("Sur les",num_ligne,"du fichier",totalValue,"lignes sont utilisée(s) pour le diagramme suivant")
+
+plt.pie(sizes, labels=labels,  autopct='%1.1f%%', shadow=True, startangle=90)
+
+plt.axis('equal')
+
+plt.savefig('PieChart.png')
+plt.show()
