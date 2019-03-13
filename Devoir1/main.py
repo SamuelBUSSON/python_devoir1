74896b7e9f3416966226abed52a6dd1071ac2613
Samuel Busson <samuel.busson@hotmail.fr>
2019-03-13 16:16:32 -0400

    Round value

--- Devoir1/main.py
+++ Devoir1/main.py
@@ -1,14 +1,19 @@
-import numpy as np
 import pandas as pd
+import time
+
+import numpy as np
 import matplotlib.pyplot as plt
+
+from sklearn import cluster, datasets, metrics
 from sklearn.cluster import DBSCAN
+from sklearn.metrics import euclidean_distances
+from sklearn.neighbors import kneighbors_graph
 from sklearn.preprocessing import StandardScaler
-import plotly.graph_objs as go
-import plotly.plotly as py
 
 file = pd.read_csv("crimeMontreal.csv", encoding = "ISO-8859-1")
 num_ligne = file.shape[0]
 num_column = file.shape[1]
+
 
 print("Le fichier possède", num_ligne, "observations et", file.shape[1], "propriétés")
 
@@ -20,6 +25,12 @@ for column in file:
 
 print("\nPBQ correspond au numéro du poste de police, aucun poste n'a le numéro -1, on remplace donc par ce dernier\n")
 file = file.fillna(-1)
+file = file[(file != 0).all(1)]
+file['X'] = file['X'].apply(lambda x: x*0.0001)
+file['Y'] = file['Y'].apply(lambda x: x*0.0001)
+file = file.round(1)
+
+
 
 print("Pour plus de clarté on remplace \"dans / sur\" par \"du contenu\"  \n")
 file["CATEGORIE"] = file["CATEGORIE"].str.replace('dans / sur véhicule à moteur', 'du contenu d\'un véhicule')
@@ -35,12 +46,12 @@ for lab in labels:
 
 print("Sur les",num_ligne,"du fichier",totalValue,"lignes sont utilisée(s) pour le diagramme suivant")
 
-plt.pie(sizes, labels=labels,  autopct='%1.1f%%', shadow=True, startangle=90)
-
-plt.axis('equal')
-
-plt.savefig('PieChart.png')
-plt.show()
+# plt.pie(sizes, labels=labels,  autopct='%1.1f%%', shadow=True, startangle=90)
+#
+# plt.axis('equal')
+#
+# plt.savefig('PieChart.png')
+# plt.show()
 
 
 
@@ -53,8 +64,8 @@ labels_true = file["CATEGORIE"].unique();
 
 
 # Now we can save it to a numpy array.
-file = file[["CATEGORIE" , "QUART", "PDQ"]]
-
+file = file[["CATEGORIE" , "QUART", "PDQ", "X", "Y"]]
+# file = file[["CATEGORIE" , "QUART"]]
 
 categorie = file["CATEGORIE"].unique()
 categorieInt = file["CATEGORIE"].unique()
@@ -73,6 +84,7 @@ for cat in categorie:
 
 print("\n===================================== \n\nOn remplace les jours, nuit,... par des numéros :")
 i = 0;
+
 for qua in quart:
     print(qua, "aura pour valeur", i)
     quartInt[i] = i
@@ -84,64 +96,44 @@ file["QUART"] = file["QUART"].astype(int)
 file["CATEGORIE"] = file["CATEGORIE"].astype(int)
 
 
-print(file)
-X = file.tail(int(num_ligne/10))
+#print(file)
+X = file.tail(int(num_ligne/20))
 
-# X = StandardScaler().fit_transform(X)
-# 
-#
-# db = DBSCAN(eps=0.3, min_samples=10).fit(X)
-# core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
-# core_samples_mask[db.core_sample_indices_] = True
-# labels = db.labels_
-#
-# n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-#
-# print('Estimated number of clusters: %d' % n_clusters_)
-#
-#
-# def matplotlib_to_plotly(cmap, pl_entries):
-#     h = 1.0 / (pl_entries - 1)
-#     pl_colorscale = []
-#
-#     for k in range(pl_entries):
-#         C = map(np.uint8, np.array(cmap(k * h)[:3]) * 255)
-#         pl_colorscale.append([k * h, 'rgb' + str((C[0], C[1], C[2]))])
-#
-#     return pl_colorscale
-#
-#
-# unique_labels = set(labels)
-#
-# colors = matplotlib_to_plotly(plt.cm.Spectral, len(unique_labels))
-# data = []
-#
-# for k, col in zip(unique_labels, colors):
-#
-#     if k == -1:
-#         # Black used for noise.
-#         col = 'black'
-#     else:
-#         col = col[1]
-#
-#     class_member_mask = (labels == k)
-#
-#     xy = X[class_member_mask & core_samples_mask]
-#     trace1 = go.Scatter(x=xy[:, 0], y=xy[:, 1], mode='markers',
-#                         marker=dict(color=col, size=14,
-#                                     line=dict(color='black', width=1)))
-#
-#     xy = X[class_member_mask & ~core_samples_mask]
-#     trace2 = go.Scatter(x=xy[:, 0], y=xy[:, 1], mode='markers',
-#                         marker=dict(color=col, size=14,
-#                                     line=dict(color='black', width=1)))
-#     data.append(trace1)
-#     data.append(trace2)
-#
-# layout = go.Layout(showlegend=False,
-#                    title='Estimated number of clusters: %d' % n_clusters_,
-#                    xaxis=dict(showgrid=False, zeroline=False),
-#                    yaxis=dict(showgrid=False, zeroline=False))
-# fig = go.Figure(data=data, layout=layout)
-#
-# py.iplot(fig)
+df2 = file["CATEGORIE"].groupby(file["X"]).size()
+print(df2)
+df3 = file["Y"]
+df1 = file["X"].unique()
+
+X = np.vstack((df1, df2)).T
+
+db = DBSCAN(eps=150, min_samples=2).fit(X)
+
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+# Number of clusters in labels, ignoring noise if present.
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+print('Estimated number of clusters: %d' % n_clusters_)
+print('Estimated number of noise points: %d' % n_noise_)
+
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+for each in np.linspace(0, 1, len(unique_labels))]
+
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+    xy = X[class_member_mask & core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=14)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy[:, 0], xy[:, 1],  'o', markerfacecolor=tuple(col), markeredgecolor='k', markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
